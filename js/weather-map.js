c0f0270c47b2a88bbd5247c62093a60bd12f0b0e
becca <r.alexis.tolentino@gmail.com>
2017-09-07 15:38:29 -0500

    add stylesheets functioning

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,22 +1,28 @@
 (function () {
     "use strict";
-    var request = $.ajax({
-        url: "http://api.openweathermap.org/data/2.5/forecast",
-        type: "GET",
-        data: {
-            APPID: "2551290df502c3718db65b75d1ba26cf",
-            id: "4726206",
-            units: "imperial",
-            cnt:3
-        }
-    });
-    request.done(success);
-    request.fail(failure);
+
+    function start(lat, lon) {
+        var request = $.ajax({
+            url: "http://api.openweathermap.org/data/2.5/forecast",
+            type: "GET",
+            data: {
+                APPID: "2551290df502c3718db65b75d1ba26cf",
+                lat: lat,
+                lon: lon,
+                units: "imperial",
+                cnt:3
+            }
+        });
+        request.done(success);
+        request.fail(failure);
+    }
+
+   start(29.42,-98.485);
 
     function success(data, status, jqXhr) {
         // alert("Request established");
 
-        console.log(data.list);
+        console.log(data);
         weatherInfo(data.list);
     }
 
@@ -38,9 +44,9 @@
             var url= "http://openweathermap.org/img/w/"+day.weather[0].icon+".png";
             var img="<img src='"+url+"'>";
             var degree =
-                "<div class='day'>"
+                "<div class='day col-md-4 box'>"
                 +"<ul>"
-                +"<li>" + day.main.temp_max + "ºF" + "/" + day.main.temp_min + "ºF" +"</li>"
+                +"<li>" + "<strong>"+Math.round(day.main.temp_max)+"</strong>" + "<strong>ºF</strong>" +"<strong>/</strong>" + "<strong>"+Math.round(day.main.temp_min)+"</strong>" + '<strong>ºF</strong>' +"</li>"
                 +"<li>"+img +"</li>"
                 +"<li>"+"<strong>Clouds:</strong>"+ day.weather[0].description+"</li>"
                 +"<li>"+"<strong>Humidity:</strong>"+day.main.humidity +"</li>"
@@ -48,7 +54,9 @@
                 +"<li>"+"<strong>Pressure:</strong>"+day.main.pressure +"</li>"
                 +"</ul>"+
                 "</div>";
-            $('body').append(degree);
+            $('#info-dump').append(degree);
+
+
 
         });
 
