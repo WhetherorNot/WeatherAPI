b1c88b9da872be1cc38c3a32851e841c95b2d357
becca <r.alexis.tolentino@gmail.com>
2017-09-07 11:26:14 -0500

    create function to list forecast 3 days

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,28 +1,42 @@
-(function(){
+(function () {
     "use strict";
-    var request=$.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather",
+    var request = $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/forecast",
         type: "GET",
         data: {
             APPID: "2551290df502c3718db65b75d1ba26cf",
             id: "4726206",
-            units:"imperial"
+            units: "imperial",
+            cnt:3
         }
     });
-        request.done(success);
-        request.fail(failure);
+    request.done(success);
+    request.fail(failure);
 
-    function success(data,status,jqXhr) {
-            alert("Request established");
-            console.log(data);
-        weatherInfo(data);
+    function success(data, status, jqXhr) {
+        // alert("Request established");
 
+        console.log(data.list);
+        weatherInfo(data.list);
     }
-    function failure(jqXhr, status, error){
+
+    function failure(jqXhr, status, error) {
         alert("Request failed");
     }
-    function weatherInfo(data){
-        var icon=data.weather.icon);
+
+    function weatherInfo(day) {
+        // var icon = data.weather.icon;
+        // var sanAntonio = data.name;
+        // var high = day.main.temp_max;
+        // var low = day.main.temp_min;
+        // var humidity = data.main.humidity;
+        // var wind = data.wind.speed;
+        // var pressure = data.main.pressure;
+
+        day.forEach(function(day){
+            var degree = day.main.temp_max + "ºF" + "/" + day.main.temp_min + "ºF";
+            $("#day1").append(degree);
+        });
 
     }
 
