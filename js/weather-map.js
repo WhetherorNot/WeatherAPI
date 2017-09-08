99bad13b59d29bd18af99a4e26734185e9b4a67a
adrianperez <adrian.perez.456@gmail.com>
2017-09-08 11:12:15 -0500

    Added a google maps api to the weather to set location based off of lat and long.

--- js/weather-map.js
+++ js/weather-map.js
@@ -5,10 +5,7 @@
         lat: 29.423017,
         lon: -98.48527
     };
-    var mapPlace = {
-        lat: 29.423017,
-        lng: -98.48527
-    };
+
 
     function start(lat, lon) {
         var request = $.ajax({
@@ -19,50 +16,50 @@
                 lat: lat,
                 lon: lon,
                 units: "imperial",
-                cnt:3
+                cnt: 3
             }
         });
         request.done(success);
         request.fail(failure);
     }
+
     start(place.lat, place.lon);
+    googleAPI(place.lat, place.lon);
 
 
     function success(data, status, jqXhr) {
         // console.log(data);
         $('#info-dump').html("");
         $('#city').html('');
-        weatherInfo(data.list,data);
+        weatherInfo(data.list, data);
     }
 
     function failure(jqXhr, status, error) {
         alert("Request failed");
     }
 
-    function weatherInfo(day,data) {
-        var location=data.city.name;
+    function weatherInfo(day, data) {
+        var location = data.city.name;
         $('#city').append(location);
 
-        day.forEach(function(day){
+        day.forEach(function (day) {
 
-            var url= "http://openweathermap.org/img/w/"+day.weather[0].icon+".png";
-            var img="<img src='"+url+"'>";
+            var url = "http://openweathermap.org/img/w/" + day.weather[0].icon + ".png";
+            var img = "<img src='" + url + "'>";
 
             var degree =
                 "<div class='day col-md-4 box'>"
-                +"<ul>"
-                +"<li>" +"<span class=temp><strong>"+Math.round(day.main.temp_max)+"</strong>" + "<strong>ºF</strong>" +"<strong>/</strong>" + "<strong>"+Math.round(day.main.temp_min)+"</strong>" + '<strong>ºF</strong>' +"</span></li>"
-                +"<li>"+img +"</li>"
-                +"<li>"+"<strong>Clouds:</strong>"+ day.weather[0].description+"</li>"
-                +"<li>"+"<strong>Humidity:</strong>"+day.main.humidity +"</li>"
-                +"<li>"+"<strong>Wind:</strong>"+day.wind.speed +"</li>"
-                +"<li>"+"<strong>Pressure:</strong>"+day.main.pressure +"</li>"
-                +"</ul>"+
+                + "<ul>"
+                + "<li>" + "<span class=temp><strong>" + Math.round(day.main.temp_max) + "</strong>" + "<strong>ºF</strong>" + "<strong>/</strong>" + "<strong>" + Math.round(day.main.temp_min) + "</strong>" + '<strong>ºF</strong>' + "</span></li>"
+                + "<li>" + img + "</li>"
+                + "<li>" + "<strong>Clouds:</strong>" + day.weather[0].description + "</li>"
+                + "<li>" + "<strong>Humidity:</strong>" + day.main.humidity + "</li>"
+                + "<li>" + "<strong>Wind:</strong>" + day.wind.speed + "</li>"
+                + "<li>" + "<strong>Pressure:</strong>" + day.main.pressure + "</li>"
+                + "</ul>" +
                 "</div>";
 
             $('#info-dump').append(degree);
-
-
 
 
         });
@@ -70,17 +67,35 @@
     }
 
     $('button').click(function () {
-        var lat=$('#latitude').val();
-        var lon=$('#longitude').val()*-1;
+        var lat = Number($('#latitude').val());
+        var lon = Number($('#longitude').val() * -1);
+    console.log(lat);
+    console.log(lon);
 
-        start(lat,lon)
+        start(lat, lon);
+        googleAPI(lat,lon);
 
     });
+
+
+    function googleAPI(lat, lng) {
+        var mapPlace={
+            lat:lat,
+            lng:lng
+        };
+
     var mapOptions = {
-        zoom:19,
+        zoom: 19,
         center: mapPlace
+
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
+
+    var marker = new google.maps.Marker({
+        position: mapPlace,
+        map: map
+    });
+}
 
 })();
